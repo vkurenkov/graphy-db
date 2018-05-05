6b5aec28611cd531ade488bc74fefbdd5e9534d8
Vladislav Kurenkov <castielblak@gmail.com>
2018-05-05 19:34:52 +0300

    Add handler for ParseObjects message

---
+++ examples/2d-primitives/server/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public enum MessageType : byte { ParseObjects = 0, GetNeighbors = 1, Between = 2 };
+
+public class ParseObjectsMessage
+{
+    public struct PrimitiveObject
+    {
+        public float X;
+        public float Y;
+        public string Shape;
+        public string Color;
+
+        public override string ToString()
+        {
+            return X.ToString() + "; " + Y.ToString() + "; " + Shape + "; " + Color + ";";
+        }
+    }
+
+    public List<PrimitiveObject> Objects;
+}
--- examples/2d-primitives/server/Program.cs
+++ examples/2d-primitives/server/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+        var port = 9001;
+        var server = new UdpClient(port);
+
+        for (int i = 0; ; i++)
+        {
+            var client = new IPEndPoint(IPAddress.Any, port);
+            var data = server.Receive(ref client);
+            HandleMessage(data);
+        }
+    }
+
+    private static void HandleMessage(byte[] message)
+    {
+        if (message[0] >= Enum.GetNames(typeof(MessageType)).Length)
+        {
+            Console.WriteLine("Message type (" + message[0].ToString() + " is not supported.)");
+            return;
+        }
+
+        // First byte is the message type
+        var messageType = (MessageType)message[0];
+        var decodedMessage = Encoding.UTF8.GetString(message.Skip(1).ToArray());
+
+        if (messageType == MessageType.ParseObjects)
+        {
+            var deserializedMessage = JsonConvert.DeserializeObject<ParseObjectsMessage>(decodedMessage);
+            Console.WriteLine(deserializedMessage.Objects[0]);
+        }
+        else if(messageType == MessageType.GetNeighbors)
+        {
+            throw new NotImplementedException();
+        }
+        else if(messageType == MessageType.Between)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

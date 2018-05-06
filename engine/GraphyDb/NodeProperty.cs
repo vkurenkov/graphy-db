45afbc1a6ef616b1ee1bbe7cef3f284ab0d13305
Andrey Kulagin <mr.and.kul@gmail.com>
2018-05-06 14:29:03 +0300

    Convert NodeBlock to Node, Add Methods to Pull Relations

--- engine/GraphyDb/NodeProperty.cs
+++ engine/GraphyDb/NodeProperty.cs
@@ -1,8 +1,14 @@
-﻿namespace GraphyDb
+﻿using GraphyDb.IO;
+
+namespace GraphyDb
 {
     public class NodeProperty : Property
     {
         public NodeProperty(Node node, string key, object value) : base(node, key, value)
+        {
+        }
+
+        public NodeProperty(Node node, PropertyBlock propertyBlock) : base(node, propertyBlock)
         {
         }
     }
--- engine/GraphyDb/Relation.cs
+++ engine/GraphyDb/Relation.cs
@@ -1,40 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using GraphyDb.IO;
 
 namespace GraphyDb
 {
     public class Relation : Entity, IEquatable<Relation>
     {
-        public bool Equals(Relation other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return RelationId == other.RelationId;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Relation) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return RelationId;
-        }
-
-        public static bool operator ==(Relation left, Relation right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Relation left, Relation right)
-        {
-            return !Equals(left, right);
-        }
-
         public int RelationId;
 
         public Node From;
@@ -43,7 +14,7 @@ namespace GraphyDb
         public int LabelId;
         public string Label;
 
-        private readonly Dictionary<string, RelationProperty> properties;
+        public readonly Dictionary<string, RelationProperty> Properties;
 
         public Relation(Node from, Node to, string label, EntityState state)
         {
@@ -62,20 +33,78 @@ namespace GraphyDb
 
             Db = from.Db;
 
-            properties = new Dictionary<string, RelationProperty>();
+            Properties = new Dictionary<string, RelationProperty>();
 
             State = state;
             if (state != EntityState.Unchanged)
                 Db.ChangedEntities.Add(this);
         }
 
+
+        public Relation(Node from, Node to, EdgeBlock relationBlock)
+        {
+            RelationId = relationBlock.EdgeId;
+
+            LabelId = relationBlock.LabelId;
+            Label = DbReader.ReadGenericStringBlock(DbControl.LabelPath, LabelId).Data;
+
+            Properties = new Dictionary<string, RelationProperty>();
+
+            var propertyBlock = DbReader.ReadPropertyBlock(DbControl.EdgePropertyPath, relationBlock.NextProperty);
+
+            while (propertyBlock.Id != 0)
+            {
+                if (!propertyBlock.Used)
+                {
+                    propertyBlock = DbReader.ReadPropertyBlock(DbControl.EdgePropertyPath, propertyBlock.NextProperty);
+                    continue;
+                }
+
+                var property = new RelationProperty(this, propertyBlock);
+                Properties.Add(property.Key, property);
+
+                propertyBlock = DbReader.ReadPropertyBlock(DbControl.EdgePropertyPath, propertyBlock.NextProperty);
+            }
+
+
+            From = from;
+            To = to;
+
+            Db = null;
+            if (From != null)
+            {
+                Db = From.Db;
+            } else if (To != null)
+            {
+                Db = To.Db;
+            }
+
+
+            if (From == null)
+            {
+                From = new Node(DbReader.ReadNodeBlock(relationBlock.FirstNode), Db);
+            }
+
+            if (To == null)
+            {
+                To = new Node(DbReader.ReadNodeBlock(relationBlock.SecondNode), Db);
+            }
+
+        }
+
+
+
+
+
+
+
         public object this[string key]
         {
-            get => properties[key].Value;
+            get => Properties[key].Value;
 
             set
             {
-                if (properties.TryGetValue(key, out var property))
+                if (Properties.TryGetValue(key, out var property))
                 {
                     // modifying existing property:
                     property.Value = value;
@@ -83,11 +112,42 @@ namespace GraphyDb
                 else
                 {
                     // adding new property:
-                    properties[key] = new RelationProperty(this, key, value);
+                    Properties[key] = new RelationProperty(this, key, value);
                 }
             }
         }
 
+
+
+        public bool Equals(Relation other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return RelationId == other.RelationId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Relation)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return RelationId;
+        }
+
+        public static bool operator ==(Relation left, Relation right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Relation left, Relation right)
+        {
+            return !Equals(left, right);
+        }
 
     }
 }

b6b9374a85944de1bea1bf93dbabee15cdd1725e
Vladislav Kurenkov <castielblak@gmail.com>
2018-05-06 19:04:34 +0300

    Add position truncation and display of the position in 2d-primitives-example

--- examples/2d-primitives/client/camera/Assets/Scripts/PrimitiveObject.cs
+++ examples/2d-primitives/client/camera/Assets/Scripts/PrimitiveObject.cs
@@ -7,12 +7,19 @@ public class PrimitiveObject : MonoBehaviour {
     public string ToJson()
     {
         var packed = new Packed();
-        packed.X = this.transform.position.x;
-        packed.Y = this.transform.position.y;
+        packed.X = this.transform.position.x.Truncate(2);
+        packed.Y = this.transform.position.y.Truncate(2);
         packed.Shape = this.Shape;
         packed.Color = this.Color;
 
         return JsonUtility.ToJson(packed);
+    }
+
+    private void Update()
+    {
+        this.transform.GetComponentInChildren<TextMesh>().text =
+            "X: " + this.transform.position.x.Truncate(2).ToString() + "\n" +
+            "Y: " + this.transform.position.y.Truncate(2).ToString();
     }
 
     private struct Packed
